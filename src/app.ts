2b1a0eb082543788d3d99d8c745bc2b616f5caca
Ulysse Prygiel <ulysse.prygiel@gmail.com>
2021-06-24 18:42:25 +0200

    remove unneded requires syntax

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'; // We need this in order to use @Decorators
 
 import config from './config';
+import loaders from './loaders';
 
 import express from 'express';
 
@@ -9,13 +10,7 @@ import Logger from './loaders/logger';
 async function startServer() {
   const app = express();
 
-  /**
-   * A little hack here
-   * Import/Export can only be used in 'top-level code'
-   * Well, at least in node 10 without babel and at the time of writing
-   * So we are using good old require.
-   **/
-  await require('./loaders').default({ expressApp: app });
+  await loaders({ expressApp: app });
 
   app
     .listen(config.port, () => {
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,6 +2,7 @@ import expressLoader from './express';
 import dependencyInjectorLoader from './dependencyInjector';
 import mongooseLoader from './mongoose';
 import Logger from './logger';
+import UserModel from '../models/user';
 //We have to import at least all the events once so they can be triggered
 import './events';
 
@@ -19,8 +20,7 @@ export default async ({ expressApp }) => {
 
   const userModel = {
     name: 'userModel',
-    // Notice the require syntax and the '.default'
-    model: require('../models/user').default,
+    model: UserModel,
   };
 
   await dependencyInjectorLoader({
